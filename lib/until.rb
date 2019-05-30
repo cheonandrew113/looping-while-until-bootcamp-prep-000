8ae7d7e5c4024d96fcefabfeb2ac028eb33c7515
Andnrew Cheon <cheonandrew113@gmail.com>
2019-05-30 18:10:06 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,6 @@
 def using_until
-  levitation_force = 6
-  while levitation_force < 10
-    puts "Wingardium leviosa"
+  until levitation_force = 6
+  puts "Wingardium leviosa"
     levitation_force += 1
 end
 
